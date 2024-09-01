0f94de7f442ae43471471b8ed0853e89ee0deec9
Indika Viraj <samangoonerathne@gmail.com>
2024-09-01 23:03:18 +0530

    Signed-off-by: Indika Viraj <samangoonerathne@gmail.com>

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 client_config = {
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--blink-settings=imagesEnabled=false', ],
     },
     authStrategy: new LocalAuth({
         clientId: "wauserbot"
